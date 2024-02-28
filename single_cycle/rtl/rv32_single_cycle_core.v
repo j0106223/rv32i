2856a84d661dc53ccf02d34b9b62224a81504ae0
JackYeh <j0106223@gmail.com>
2024-02-28 16:01:20 +0800

    add pc logic

--- single_cycle/rtl/rv32_single_cycle_core.v
+++ single_cycle/rtl/rv32_single_cycle_core.v
@@ -1,7 +1,21 @@
 module rv32_single_cycle_core (
+    clk,
+    reset_n,
     address,
     data,
 );
-    //instruction master
-    //data master
+    parameter [31:0] RESET_VECTOR = 32h'0000_0000;
+    input clk;
+    input reset_n;
+
+    wire pc_nx;
+    assign pc_nx = (branch && zero) ? pc+4 : pc+offset;
+    reg [31:0] pc;
+    always @(posedge clk or negedge reset_n) begin
+        if(!reset_n)begin
+            pc <= RESET_VECTOR;
+        end else begin
+            pc <= pc_nx;
+        end
+    end
 endmodule
