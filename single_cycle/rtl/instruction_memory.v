9873dc59f6cf7cde828db3501a744213c4936d04
JackYeh <j0106223@gmail.com>
2024-03-04 23:53:18 +0800

    fix instruction output data

--- single_cycle/rtl/instruction_memory.v
+++ single_cycle/rtl/instruction_memory.v
@@ -13,5 +13,8 @@ module instruction_memory(
         $readmemh("a.hex",mem);
     end
 
-    assign data = mem[address];
+    assign data[31:24] = mem[address];
+    assign data[23:16] = mem[address+1];
+    assign data[15:8]  = mem[address+2];
+    assign data[7:0]   = mem[address+3];
 endmodule
