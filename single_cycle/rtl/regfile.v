5ca4ee80b09079bceb04566b96ab57c3d26f561a
JackYeh <j0106223@gmail.com>
2024-03-07 01:35:01 +0800

    in order to fit modelsim/questa coding style

--- single_cycle/rtl/regfile.v
+++ single_cycle/rtl/regfile.v
@@ -10,14 +10,14 @@ module regfile (
     wd
 );
 
-    input           clk;
-    input   [4:0]   ra1;        //read1 address
-    input   [4:0]   ra2;        //read2 address
-    output  [31:0]  rd1;        //read1 data
-    output  [31:0]  rd2;        //read2 data
-    input           write;
-    input   [4:0]   wa;         //write address
-    input   [31:0]  wd;         //write data
+    input  wire          clk;
+    input  wire   [4:0]  ra1;        //read1 address
+    input  wire   [4:0]  ra2;        //read2 address
+    output wire  [31:0]  rd1;        //read1 data
+    output wire  [31:0]  rd2;        //read2 data
+    input  wire          write;
+    input  wire   [4:0]  wa;         //write address
+    input  wire  [31:0]  wd;         //write data
 
     reg [31:0]regiser [1:31];//regiser[0] hard-wire to zero
     assign rd1 = (ra1 == 0) ? 32'h0000_0000 : regiser[ra1];//regiser[0] hard-wired to zero
