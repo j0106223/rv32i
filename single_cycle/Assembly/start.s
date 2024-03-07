6330c50db1a08675db5e9bc63c01f9865d2fff72
JackYeh <j0106223@gmail.com>
2024-03-08 00:48:31 +0800

    modified start code replace addi to add because addi is not supported

--- single_cycle/Assembly/start.s
+++ single_cycle/Assembly/start.s
@@ -2,36 +2,36 @@
 	.global _start
     .global main
 _start:
-    addi    x1, x0, 0
-    addi    x2, x0, 0
-    addi    x3, x0, 0
-    addi    x4, x0, 0
-    addi    x5, x0, 0
-    addi    x6, x0, 0
-    addi    x7, x0, 0
-    addi    x8, x0, 0
-    addi    x9, x0, 0
-    addi    x10, x0, 0
-    addi    x11, x0, 0
-    addi    x12, x0, 0
-    addi    x13, x0, 0
-    addi    x14, x0, 0
-    addi    x15, x0, 0
-    addi    x16, x0, 0
-    addi    x17, x0, 0
-    addi    x18, x0, 0
-    addi    x19, x0, 0
-    addi    x20, x0, 0
-    addi    x21, x0, 0
-    addi    x22, x0, 0
-    addi    x23, x0, 0
-    addi    x24, x0, 0
-    addi    x25, x0, 0
-    addi    x26, x0, 0
-    addi    x27, x0, 0
-    addi    x28, x0, 0
-    addi    x29, x0, 0
-    addi    x30, x0, 0
-    addi    x31, x0, 0
+    add    x1,  x0, x0
+    add    x2,  x0, x0
+    add    x3,  x0, x0
+    add    x4,  x0, x0
+    add    x5,  x0, x0
+    add    x6,  x0, x0
+    add    x7,  x0, x0
+    add    x8,  x0, x0
+    add    x9,  x0, x0
+    add    x10, x0, x0
+    add    x11, x0, x0
+    add    x12, x0, x0
+    add    x13, x0, x0
+    add    x14, x0, x0
+    add    x15, x0, x0
+    add    x16, x0, x0
+    add    x17, x0, x0
+    add    x18, x0, x0
+    add    x19, x0, x0
+    add    x20, x0, x0
+    add    x21, x0, x0
+    add    x22, x0, x0
+    add    x23, x0, x0
+    add    x24, x0, x0
+    add    x25, x0, x0
+    add    x26, x0, x0
+    add    x27, x0, x0
+    add    x28, x0, x0
+    add    x29, x0, x0
+    add    x30, x0, x0
+    add    x31, x0, x0
     jal     x0, main  # jump to main
     
